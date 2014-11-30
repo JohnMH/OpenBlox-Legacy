aca4fff4b55453ec8006c33bb339d6eeb2f770f2
John M. Harris, Jr <jharris@myzillawr.com>
2014-11-30 12:09:37 -0500

    Vector3. Yeah. Yeeeeeeeeeeah.

--- src/ob_type/Vector3.cpp
+++ src/ob_type/Vector3.cpp
@@ -1,21 +1,103 @@
-/*
- * Vector3.cpp
- *
- *  Created on: Nov 30, 2014
- *      Author: john
- */
-
 #include "Vector3.h"
 
-namespace ob_instance {
-
+namespace ob_instance{
 	Vector3::Vector3(){
-		// TODO Auto-generated constructor stub
-
+		x = 0;
+		y = 0;
+		z = 0;
+		magnitude = 1;
 	}
 
-	Vector3::~Vector3(){
-		// TODO Auto-generated destructor stub
+	Vector3::Vector3(double x, double y, double z){
+		this->x = x;
+		this->y = y;
+		this->z = z;
+		magnitude = sqrt(dot(this));
 	}
 
-} /* namespace ob_instance */
+	Vector3::~Vector3(){}
+
+	Vector3* Vector3::getNormalized(){
+		Vector3* newGuy = new Vector3();
+		newGuy->x = x;
+		newGuy->y = y;
+		newGuy->z = z;
+		return newGuy;
+	}
+
+	Vector3* Vector3::add(double v){
+		return new Vector3(x + v, y + v, z + v);
+	}
+
+	Vector3* Vector3::add(Vector3* other){
+		if(other == NULL){
+			return NULL;
+		}
+		return new Vector3(x + other->x, y + other->y, z + other->z);
+	}
+
+	Vector3* Vector3::sub(double v){
+		return new Vector3(x - v, y - v, z - v);
+	}
+
+	Vector3* Vector3::sub(Vector3* other){
+		if(other == NULL){
+			return NULL;
+		}
+		return new Vector3(x - other->x, y - other->y, z - other->z);
+	}
+
+	Vector3* Vector3::mul(double v){
+		return new Vector3(x * v, y * v, z * v);
+	}
+
+	Vector3* Vector3::mul(Vector3* other){
+		if(other == NULL){
+			return NULL;
+		}
+		return new Vector3(x * other->x, y * other->y, z * other->z);
+	}
+
+	Vector3* Vector3::div(double v){
+		return new Vector3(x / v, y / v, z / v);
+	}
+
+	Vector3* Vector3::div(Vector3* other){
+		if(other == NULL){
+			return NULL;
+		}
+		return new Vector3(x / other->x, y / other->y, z / other->z);
+	}
+
+	Vector3* Vector3::neg(){
+		return new Vector3(-x, -y, -z);
+	}
+
+	Vector3* Vector3::lerp(Vector3* goal, double alpha){
+		if(goal == NULL){
+			return NULL;
+		}
+		return add(alpha)->mul(goal->sub(this));
+	}
+
+	double Vector3::dot(Vector3* other){
+		if(other == NULL){
+			return 0;
+		}
+		return x*other->x + y*other->y + z*other->z;
+	}
+
+	Vector3* Vector3::cross(Vector3* other){
+		if(other == NULL){
+			return NULL;
+		}
+		return new Vector3(y*other->z - z*other->y, x*other->z - other->z*x, x*other->y - other->y*x);
+	}
+
+	bool Vector3::isClose(Vector3* other, double epsilon){
+		if(other == NULL){
+			return NULL;
+		}
+		return sub(other)->getMagnitude() <= epsilon;
+	}
+}
--- src/ob_type/Vector3.h
+++ src/ob_type/Vector3.h
@@ -1,21 +1,38 @@
-/*
- * Vector3.h
- *
- *  Created on: Nov 30, 2014
- *      Author: john
- */
-
 #ifndef OB_TYPE_VECTOR3_H_
 #define OB_TYPE_VECTOR3_H_
 
-namespace ob_instance {
+#include "OpenBlox.h"
+#include <cmath>
 
+namespace ob_instance{
 	class Vector3{
 		public:
 			Vector3();
+			Vector3(double x, double y, double z);
 			virtual ~Vector3();
+
+			double x;
+			double y;
+			double z;
+			double magnitude;
+
+			Vector3* getNormalized();//unit
+
+			Vector3* add(double v);
+			Vector3* add(Vector3* other);
+			Vector3* sub(double v);
+			Vector3* sub(Vector3* other);
+			Vector3* mul(double v);
+			Vector3* mul(Vector3* other);
+			Vector3* div(double v);
+			Vector3* div(Vector3* other);
+			Vector3* neg();
+
+			Vector3* lerp(Vector3* goal, double alpha);
+			double dot(Vector3* other);
+			Vector3* cross(Vector3* other);
+			bool isClose(Vector3* other, double epsilon = 1e-6);
 	};
 
-} /* namespace ob_instance */
-
-#endif /* OB_TYPE_VECTOR3_H_ */
+}
+#endif
