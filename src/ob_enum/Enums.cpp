eecf14a4d18a6a81c4381378dcde22641756edd3
John M. Harris, Jr. <jharris@myzillawr.com>
2014-12-28 21:29:32 -0500

    Removed TODO.txt, added Enum HttpContentType

--- src/ob_enum/Enums.cpp
+++ src/ob_enum/Enums.cpp
@@ -2,8 +2,8 @@
 
 namespace ob_enum{
 	LuaEnum* LuaFormFactor = new LuaEnum("FormFactor", 4, "Symmetric", "Brick", "Plate", "Custom");
-
 	LuaEnum* LuaCameraType = new LuaEnum("CameraType", 7, "Fixed", "Attach", "Watch", "Track", "Follow", "Custom", "Scriptable");
+	LuaEnum* LuaHttpContentType = new LuaEnum("HttpContentType", 5, "ApplicationJson", "ApplicationXml", "ApplicationUrlEncoded", "TextPlain", "TextXml");
 
 	void registerEnums(lua_State* L){
 		lua_newtable(L);
--- src/ob_enum/Enums.h
+++ src/ob_enum/Enums.h
@@ -8,19 +8,26 @@ namespace ob_enum{
 	extern LuaEnum* LuaCameraType;
 
 	enum class FormFactor{
-		SYMMETRIC,
-		BRICK,
-		PLATE,
-		CUSTOM
+		SYMMETRIC = 0,
+		BRICK = 1,
+		PLATE = 2,
+		CUSTOM = 3
 	};
 	enum class CameraType{
-		FIXED,
-		ATTACH,
-		WATCH,
-		TRACK,
-		FOLLOW,
-		CUSTOM,
-		SCRIPTABLE
+		FIXED = 0,
+		ATTACH = 1,
+		WATCH = 2,
+		TRACK = 3,
+		FOLLOW = 4,
+		CUSTOM = 5,
+		SCRIPTABLE = 6
+	};
+	enum class HttpContentType{
+		ApplicationJson = 0,
+		ApplicationXml = 1,
+		ApplicationUrlEncoded = 2,
+		TextPlain = 3,
+		TextXml = 4
 	};
 
 	void registerEnums(lua_State* L);
